e61e61f00c2c6c1c7e01c4942944b75f52167125
teleidoskopbild <teleidoskopbild@gmail.com>
2025-03-01 20:14:18 +0100

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="grid grid-cols-3 grid-rows-3 gap-2 sm:gap-4 h-screen">
+    <div class="bg-blue-500 row-span-2">Section 1</div>
+    <div class="bg-green-500 col-span-2">Section 2</div>
+    <div class="bg-red-500">Section 3</div>
+    <div class="bg-yellow-500 row-span-2">Section 4</div>
+    <div class="bg-purple-500 col-span-2">Section 5</div>
+  </div>
+</template>
+<script setup></script>
+<style scoped></style>
